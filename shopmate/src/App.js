872bed1aff4b38df2072f8e47355533865cb95c6
pawel2557 <pawelnowak984@gmail.com>
2025-04-17 14:22:04 +0200

    added loading inormation

--- shopmate/src/App.js
+++ shopmate/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ProductList from './components/ProductList';
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <ProductList/>
     </div>
   );
 }
